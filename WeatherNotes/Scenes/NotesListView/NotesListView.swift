401a4e0fd1900e7583bbffc8971ac8d67569b837
Maksym <asasincer@gmail.com>
2026-02-06 18:53:47 +0100

    Проста вьюшка з мок данними щоб знати від чого відштовхуватись

---
+++ WeatherNotes/Scenes/NotesListView/NotesListView.swift
@@ -0,0 +1,21 @@
+//
+//  NotesListView.swift
+//  WeatherNotes
+//
+//  Created by Maks on 06.02.26.
+//
+
+
+import SwiftUI
+
+struct NotesListView: View {
+    
+    
+    var body: some View {
+        
+        Text("Notes List")
+            .navigationBarTitle("Notes")
+            .navigationBarItems(trailing: Button("Add Note") {
+
+            })   }
+}
